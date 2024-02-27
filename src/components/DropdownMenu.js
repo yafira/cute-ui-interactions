9a91ca6364b8a89eb5b866b4d93df2a104dfd03a
yafira <yafira.martinez@lc.cuny.edu>
2024-02-27 15:43:13 -0500

    changed focus color and spacing

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -19,13 +19,13 @@ const DropdownMenu = () => {
 	}
 
 	return (
-		<div className='flex justify-center mb-4'>
-			<div className='relative inline-block text-left'>
+		<div className='flex justify-center mt-8 mb-4'>
+			<div className='relative inline-block text-left mr-4'>
 				<div>
 					<button
 						onClick={toggleMenu1}
 						type='button'
-						className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500'
+						className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-200'
 					>
 						Options 1
 						<svg
@@ -77,12 +77,12 @@ const DropdownMenu = () => {
 					)}
 				</AnimatePresence>
 			</div>
-			<div className='relative inline-block text-left'>
+			<div className='relative inline-block text-left mr-4'>
 				<div>
 					<button
 						onClick={toggleMenu2}
 						type='button'
-						className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500'
+						className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-lime-200'
 					>
 						Options 2
 						<svg
@@ -139,7 +139,7 @@ const DropdownMenu = () => {
 					<button
 						onClick={toggleMenu3}
 						type='button'
-						className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500'
+						className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-violet-200'
 					>
 						Options 3
 						<svg
